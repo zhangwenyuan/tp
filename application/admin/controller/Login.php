02711cb330a48c3ab19394c93235e538abd6a1b8
zhangwenyuan <zhangwenyuan@maxrocky.com>
2018-04-26 19:01:46 +0800

    更新角色管理

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -12,19 +12,15 @@ class Login extends Controller
      * 主页
      * */
     public function login(){
-
         $_menu = model('menu');
         $menu_list = $_menu->menulist();
-
         $this->assign('menu_list',$menu_list);
-
         return view('index/index');
     }
     /**
      * 管 理 员 列 表
      * */
     public function admin_list(){
-
         $starttime = isset($_REQUEST['start'])?$_REQUEST['start']:'';
         $endtime = isset($_REQUEST['end'])?$_REQUEST['end']:'';
         $username = isset($_REQUEST['username'])?$_REQUEST['username']:'';
@@ -33,7 +29,11 @@ class Login extends Controller
         $p_limit = ($page-1)*$pageSize;//偏移量计算
         $_manage = model('user');
         $_managelist = $_manage->Administration_List($p_limit,$pageSize,$starttime,$endtime,$username);
-
+        foreach ($_managelist as $key => &$val){
+            $role_id = $_manage->getRoleUserId($val['id']);
+            $rolename = $_manage->getRoleInfo($role_id);
+            $val['role_name'] = $rolename['name'];
+        }
         $total = $_manage->Administration_List_count($username,$starttime,$endtime);
         $this->assign([
             'manage_list' => $_managelist,
@@ -48,7 +48,8 @@ class Login extends Controller
      * 编 辑 管 理 员 信 息
      * */
     public function admin_edit(){
-
+        $usermodel = model('user');
+        $rolelist = $usermodel->role_list();
         if(!empty($_POST['saveid'])){
             $usermodel = model('user');
             $_data = array();
@@ -56,21 +57,22 @@ class Login extends Controller
             $_data['email'] = isset($_POST['email'])?$_POST['email']:'';
             $_data['phone'] = isset($_POST['phone'])?$_POST['phone']:'';
             $checkname = $usermodel->checkuser($_data['name']);
-            if($checkname){
+            if($checkname && $checkname['name'] != $_data['name']){
                 return exitMsg(202,'该用户名已存在，请重新提交~');
             }
-            if($usermodel->user_save($_data,$_POST['saveid'])){
+            $res = $usermodel->user_save($_data,$_POST['saveid']);
+            if($res || $res==0){#当数据没改动的情况下(update 将返回0)
+                $usermodel->SaveUserRole($_POST['saveid']);
                 return exitMsg(200,'用户修改成功~');
             }else{
                 return exitMsg(201,'修改失败，请重新提交~');
             }
-
         }
         $saveid = $_REQUEST['id'];
         $saveinfo = model('user');
         $userinfo = $saveinfo->getUserInfo($saveid);
-
-        $this->assign('userinfo',$userinfo);
+        $userinfo['role_id'] = $usermodel->getRoleUserId($userinfo['id']);
+        $this->assign(['rolelist'=>$rolelist,'userinfo'=>$userinfo]);
         return view('index/admin_edit');
     }
     /**
@@ -91,7 +93,6 @@ class Login extends Controller
      * 暂停该管理员
      * */
     public function admin_save_status(){
-
         $id = $_POST['id'];
         if(empty($id))
             return exitMsg(202,'非法操作，无法识别的用户~');
@@ -106,7 +107,6 @@ class Login extends Controller
      * 管 理 员 添 加
      * */
     public function admin_add(){
-
         if(!empty($_POST['phone'])){
             $usermodel = model('user');
             $_data = array();
@@ -114,18 +114,25 @@ class Login extends Controller
             $_data['email'] = isset($_POST['email'])?$_POST['email']:'';
             $_data['pwd'] = isset($_POST['repass'])?$_POST['repass']:'';
             $_data['phone'] = isset($_POST['phone'])?$_POST['phone']:'';
+            if($_POST['role_id'] == 2){
+                $_data['is_admin'] = 1;
+            }
             $checkname = $usermodel->checkuser($_data['name']);
             if($checkname){
                 return exitMsg(202,'该用户名已存在，请重新提交~');
             }
             if($usermodel->user_add($_data)){
+                $userId = Db::name('user')->getLastInsID();
+                $usermodel->add_role_user($userId);
                 return exitMsg(200,'用户注册成功~');
             }else{
                 return exitMsg(201,'注册失败，请重新提交~');
             }
 
         }
-
+        $usermodel = model('user');
+        $rolelist = $usermodel->role_list();
+        $this->assign('rolelist',$rolelist);
         return view('index/admin_add');
     }
 
@@ -133,7 +140,10 @@ class Login extends Controller
      * 角 色 管 理
      * */
     public function admin_role(){
+        $usermodel = model('user');
+        $rolelist = $usermodel->role_list();
 
+        $this->assign('rolelist',$rolelist);
         return view('index/admin_role');
     }
 
@@ -141,10 +151,66 @@ class Login extends Controller
      * 角 色 添 加
      * */
     public function role_add(){
-
+        $menumodel = model('menu');
+        $usermodel = model('user');
+        if(!empty($_POST['rolename'])){
+            $_data = array();
+            $_data['name'] = isset($_POST['rolename'])?$_POST['rolename']:'';
+            $_data['desc'] = isset($_POST['desc'])?$_POST['desc']:'';
+            $_data['create_time'] = time();
+            if($usermodel->role_add($_data)){
+                $roleinsertID = $usermodel->getLastInsID();
+                $usermodel->InsertRoleAccess($roleinsertID);
+                return exitMsg(200,'角色添加成功~');
+            }else{
+                return exitMsg(201,'角色添加失败，请重新提交~');
+            }
+        }
+        $menu_list = $menumodel->menulist();
+        $this->assign('menu_list',$menu_list);
         return view('index/role_add');
     }
 
+    /**
+     * 角 色 del
+     * */
+    public function role_del(){
+        $id = $_POST['id'];
+        if(empty($id))
+            return exitMsg(202,'非法操作，无法识别的角色~');
+        $del_manage = model('user');
+        $del_res = $del_manage->del_role($id);
+        if($del_res)
+            return exitMsg(200,'用户角色成功~');
+        else
+            return exitMsg(201,'用户角色失败，请稍后重试~');
+    }
+
+    /**
+     * 角 色 修 改
+     * */
+    public function role_edit(){
+        $menumodel = model('menu');
+        if(!empty($_REQUEST['saveid'])){
+            $usermodel = model('user');
+            $_data = array();
+            $_data['name'] = isset($_POST['rolename'])?$_POST['rolename']:'';
+            $_data['desc'] = isset($_POST['desc'])?$_POST['desc']:'';
+            $_data['update_time'] = time();
+            if($usermodel->role_save($_data,$_POST['saveid'])){
+                return exitMsg(200,'角色修改成功~');
+            }else{
+                return exitMsg(201,'角色失败，请重新提交~');
+            }
+        }
+        $saveid = $_REQUEST['editid'];
+        $saveinfo = model('user');
+        $roleinfo = $saveinfo->getRoleInfo($saveid);
+        $menu_list = $menumodel->menulist();
+        $this->assign('menu_list',$menu_list);
+        $this->assign('roleinfo',$roleinfo);
+        return view('index/role_edit');
+    }
 
     public function welcome(){
 
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -34,10 +34,7 @@ class User extends Model{
     public function checkuser($_name){
 
         $_checkres = Db::name('user')->where('name',$_name)->find();
-        if($_checkres)
-            return true;
-        else
-            return false;
+        return $_checkres;
     }
     /**
      * 管理员列表
@@ -107,8 +104,121 @@ class User extends Model{
      * */
     public function user_save($data,$id){
 
+//        $save_res = Db::name('user')->where('id',$id)->update($data);
         $save_res = Db::name('user')->where('id',$id)->update($data);
         if($save_res)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * 增加角色
+     * param  $data：需要增加的信息
+     * return true/false
+     * */
+    public function role_add($data){
+
+        $insertres = Db::name('role')->insert($data);
+        if($insertres)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * 角色列表
+     * return array()
+     * */
+    public function role_list(){
+
+        $rolelist = Db::name('role')->where('status',1)->select();
+
+        return $rolelist;
+    }
+
+    /**
+     * 删除角色
+     * param $id 角色id
+     * return true/fasle
+     * */
+    public function del_role($id){
+        $del_res = Db::name('role')->where('id',$id)->delete();
+        Db::name('role_access')->where('role_id',$id)->delete();# 清除权限与角色表的关联数据
+        if($del_res)
+            return true;
+        else
+            return false;
+    }
+    /**
+     * 根据ID查询角色的信息
+     * return array()
+     * */
+    public function getRoleInfo($id){
+
+        $roleinfo = Db::name('role')->where('id',$id)->find();
+        return $roleinfo;
+    }
+
+    /**
+     * 修改角色信息
+     * param  $data：需要修改的信息   $id：用户id
+     * return true/false
+     * */
+    public function role_save($data,$id){
+
+        $save_res = Db::name('role')->where('id',$id)->update($data);
+        if($save_res)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * 增加用户角色关系
+     * */
+    public function add_role_user($uid){
+        $_data['role_id'] = isset($_POST['role_id'])?$_POST['role_id']:'';
+        $_data['uid'] = $uid;
+        $_data['create_time'] = time();
+        $insertres = Db::name('user_role')->insert($_data);
+        if($insertres)
+            return true;
+        else
+            return false;
+    }
+    /**
+     * 根据用户id查询角色id
+     * param id 用户id
+     * */
+    public function getRoleUserId($id){
+        $roleid = Db::name('user_role')->where('uid',$id)->value('role_id');
+        return $roleid;
+
+    }
+    /**
+     * 根据用户id修改角色id
+     * param id 用户id
+     * */
+    public function SaveUserRole($id){
+        $_data['role_id'] = isset($_POST['role_id'])?$_POST['role_id']:0;
+        $save_res = Db::name('user_role')->where('uid',$id)->update($_data);
+        return $save_res;
+    }
+
+    /**
+     * 增加角色权限
+     * param $roleinsertID 角色id
+     * return true/fasle
+     * */
+    public function InsertRoleAccess($roleinsertID){
+        $_data['access_str'] = isset($_POST['checkid'])?$_POST['checkid']:'';
+        $_data['access_id'] = isset($_POST['fa_checkid'])?$_POST['fa_checkid']:'';
+        $_data['role_id'] = $roleinsertID;
+        $_data['create_time'] = time();
+        $insert_res = Db::name('role_access')->insert($_data);
+        if($insert_res)
+
             return true;
         else
             return false;
